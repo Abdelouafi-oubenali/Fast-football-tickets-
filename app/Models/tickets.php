0240d6e6bf151c1bae0684e993ba9a38109372a0
abdelouafi <abdelouafioubenali.com>
2025-03-16 13:59:20 +0000

    Préparation des modèles de tickets avec les relations de ce modèle

--- app/Models/tickets.php
+++ app/Models/tickets.php
@@ -9,4 +9,23 @@ class tickets extends Model
 {
     /** @use HasFactory<\Database\Factories\TicketsFactory> */
     use HasFactory;
+    protected $fillable = [
+        'date',
+        'time',
+        'Stadium',
+        'home_team_id',
+        'away_team_id',
+        'home_team_score',
+        'away_team_score'
+    ];
+
+    public function homeTeam()
+    {
+        return $this->belongsTo(Equipe::class, 'home_team_id');
+    }
+
+    public function awayTeam()
+    {
+        return $this->belongsTo(Equipe::class, 'away_team_id');
+    }
 }
