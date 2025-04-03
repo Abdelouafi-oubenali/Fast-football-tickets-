a5bed00ed88ac25888a8ed8b0965ce8ad8d785ce
abdelouafi <abdelouafioubenali.com>
2025-04-03 22:22:08 +0000

    Diviser les fichiers index, séparer le header, la nav et le footer, et corriger les fausses phrases

---
+++ resources/views/layouts/man.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="/assets/css/output.css" rel="stylesheet">
+		<link rel="icon" type="image/png" href="/assets/icons/favicon.png">
+        <title>@yield('title')</title>
+
+		<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+		<script src="https://kit.fontawesome.com/029424212f.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+		<style>
+			.scrollbar-hide::-webkit-scrollbar {
+			  display: none;
+			}
+			
+			.scrollbar-hide {
+			  -ms-overflow-style: none;
+			  scrollbar-width: none;
+			}
+			
+			html {
+			  scroll-behavior: smooth;
+			}
+		  </style>
+
+	</head>
+<body>
+
+    <header>
+
+        @include('partials.header_man') 
+    </header>
+    
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        @include('partials.footer')
+    </footer>
+
+    
+	<script>
+		document.querySelector('.circl_nv').addEventListener('click', function() {
+			const menu = document.querySelector('.menu_nv');
+			menu.classList.toggle('hidden');
+		});
+	</script>
+
+</body>
+</html>
