e67c25ab3dd4ab1899275543f9370cc719fa481d
abdelouafi <abdelouafioubenali.com>
2025-04-05 14:38:39 +0000

    Créer une table category des places qui gère les prix et les places données pour mon match.

---
+++ database/migrations/2025_04_05_001308_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('match_id')->constrained('tickets')->onDelete('cascade');
+            $table->string('nom');
+            $table->decimal('prix', 8, 2);
+            $table->boolean('actif')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
