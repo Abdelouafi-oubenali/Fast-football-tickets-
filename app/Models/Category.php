0310e80111aa7067d14703c77b30d1af053ca215
abdelouafi <abdelouafioubenali.com>
2025-04-12 20:26:26 +0100

    ajouter la colonne numéro des places à la table des catégories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['nom', 'prix', 'prix_abonne', 'actif'];
+    protected $fillable = ['nom', 'prix', 'prix_abonne', 'actif','nombre_place'];
 
     public function match()
     {
