4502cb404c0f6f3789b536264fa167886ea1ae6b
abdelouafi <abdelouafioubenali.com>
2025-04-18 23:04:13 +0100

    FFT-72: Appel des fonctions de gestion des permissions.

--- app/Http/Controllers/DashbordController.php
+++ app/Http/Controllers/DashbordController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\statiwticControler;
 
 class DashbordController extends Controller
@@ -15,19 +16,21 @@ class DashbordController extends Controller
     
     public function __construct()
     {
+        $user = Auth::user(); 
+        $chek = $this->checkRoleAdminOrganisater($user->role);
+        if($chek){
+            abort(403);
+        }
         $this->totalTicketsController = new statiwticControler(); 
-
-
     }
 
-    public function show()
+    public function index()
     {
         $teckts = $this->totalTicketsController->TotalTickts(); 
         $matchs = $this->totalTicketsController->TotalMatchs(); 
         $Stads = $this->totalTicketsController->TotalStads();
         $Equipe = $this->totalTicketsController->TotalStads(); 
-
-
+        
         return view('admin.dashboard', compact('matchs','teckts','Stads','Equipe'));
     }
 }
