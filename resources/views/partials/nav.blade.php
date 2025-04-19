dac5f153dc24bcd85ce948552adaa1967c79d7a4
abdelouafi <abdelouafioubenali.com>
2025-04-19 13:32:30 +0100

    modification des routes vers les tickets

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -19,7 +19,7 @@
     
 
         <li>
-            <a href="{{ route('admin.vente-de-tickets') }}" class="flex items-center text-gray-300 hover:text-white py-2 px-4 rounded hover:bg-green-700 transition-all duration-200">
+            <a href="/vente-de-tickets" class="flex items-center text-gray-300 hover:text-white py-2 px-4 rounded hover:bg-green-700 transition-all duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
                 </svg>
