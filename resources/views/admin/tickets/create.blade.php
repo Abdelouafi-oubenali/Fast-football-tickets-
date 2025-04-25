4b350de91533e241d6c5dc95a7c79b0ceb950a33
abdelouafi <abdelouafioubenali9@gmail.com>
2025-04-25 22:55:15 +0100

    Merge du hotfix avec le développement

--- resources/views/admin/tickets/create.blade.php
+++ resources/views/admin/tickets/create.blade.php
@@ -73,9 +73,6 @@
                                     @endphp
                                     <label for="date_match" class="block text-gray-700 mb-1">Date du Match</label>
                                     <input type="date" id="date_match" name="date" value="{{ old('date', $tomorrow) }}" min="{{ $tomorrow }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-
-
-
                                 @error('date')
                                     <span class="text-red-500 text-sm">{{ $message }}</span>
                                 @enderror
