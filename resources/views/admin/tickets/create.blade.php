df7cf92f39c96ddd0564a5be185b4f2c542b9b4d
abdelouafi <abdelouafioubenali9@gmail.com>
2025-04-25 22:53:38 +0100

    Merge du hotfix avec le développement

--- resources/views/admin/tickets/create.blade.php
+++ resources/views/admin/tickets/create.blade.php
@@ -72,7 +72,6 @@
                                         $tomorrow = \Carbon\Carbon::tomorrow()->toDateString();
                                     @endphp
                                     <label for="date_match" class="block text-gray-700 mb-1">Date du Match</label>
-                                    {{-- <input type="date" id="date_match" name="date"  class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"> --}}
                                     <input type="date" id="date_match" name="date" value="{{ old('date', $tomorrow) }}" min="{{ $tomorrow }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
 
 
