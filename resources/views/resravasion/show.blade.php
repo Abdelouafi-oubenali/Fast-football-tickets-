01ff7857242e4e02e8b6d4a3d20315f9eb7148a6
abdelouafi <abdelouafioubenali9@gmail.com>
2025-04-26 19:50:08 +0100

    Affichage du message d'erreur dans la page d'affichage

--- resources/views/resravasion/show.blade.php
+++ resources/views/resravasion/show.blade.php
@@ -5,6 +5,14 @@
 @section('content')
 
 <main class="pt-20 p-8">
+    
+    @if (session('error'))
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Erreur ! </strong>
+            <span class="block sm:inline">{{ session('error') }}</span>
+        </div>
+    @endif
+
     <form action="{{route('reservation.store')}}" method="POST">
         @csrf
 
