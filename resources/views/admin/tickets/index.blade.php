b91876501813018f769fe42ba301c28443aa4c51
abdelouafi <abdelouafioubenali9@gmail.com>
2025-05-02 21:47:25 +0100

    Ajoute un bouton pour afficher les matchs passés.

--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -5,14 +5,22 @@
 @section('content')
   <main class="ml-64 pt-20 p-8">
     <!-- Filtres -->
-    <div class="container mx-auto px-4 py-6">
+    <div class="container mx-auto px-4 py-6 flex">
         
-        <div class="bg-white rounded-lg shadow p-4 flex flex-wrap gap-4">
-        <a href="/tickets/create" class="bg-green-600 hover:bg-green-700 text-white px-6 py-4 rounded-lg shadow-lg transition-colors duration-200 flex items-center gap-2 w-full md:w-auto justify-center">
+        <div class="bg-white rounded-lg shadow p-4 flex flex-wrap gap-[45rem]">
+        <a href="/tickets/create/" class="bg-green-600 hover:bg-green-700 text-white px-6 py-4 rounded-lg shadow-lg transition-colors duration-200 flex items-center gap-2 w-full md:w-auto justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
             </svg>
-            Ajouter un tickets
+            Ajouter un Match
+        </a>
+
+        <a href="/matchs/historique/{{1}}" class="bg-green-600 hover:bg-green-700 text-white px-6 py-4 rounded-lg shadow-lg transition-colors duration-200 flex items-center gap-2 w-full md:w-auto justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+              </svg>
+              
+          Historique
         </a>
         </div>
     
@@ -84,6 +92,7 @@
                 </div>
             </div>
           @endforeach
+          
         </div>
     </div>
   </main>
